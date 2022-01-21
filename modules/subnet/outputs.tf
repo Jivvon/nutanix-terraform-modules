44e8b047f8a9ceed7f2664494b0a9a3383ad5abb
Jeong Ji Won <jwjeong@si-analytics.ai>
2022-01-22 01:52:09 +0900

    Add output 'id, ip' on subnet module

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -3,6 +3,16 @@ output "name" {
   value = nutanix_subnet.subnet.name
 }
 
+output "id" {
+  description = "ID of the subnet"
+  value = nutanix_subnet.subnet.id
+}
+
+output "ip" {
+  description = "IP of the subnet"
+  value = nutanix_subnet.subnet.subnet_ip
+}
+
 output "network_ip_address" {
   description = "Network IP Address/Prefix Length of the subnet"
   value = "${nutanix_subnet.subnet.subnet_ip}/${nutanix_subnet.subnet.prefix_length}"
