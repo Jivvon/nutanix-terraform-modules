a35926b0d7cfdfc4706934ec15aa6944915e798d
Jeong Ji Won <jwjeong@si-analytics.ai>
2022-01-23 02:06:27 +0900

    Remove outputs don't need

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -1,19 +1,4 @@
-output "name" {
-  description = "Name of the subnet"
-  value = nutanix_subnet.subnet.name
-}
-
 output "id" {
   description = "ID of the subnet"
-  value = nutanix_subnet.subnet.id
-}
-
-output "ip" {
-  description = "IP of the subnet"
-  value = nutanix_subnet.subnet.subnet_ip
-}
-
-output "network_ip_address" {
-  description = "Network IP Address/Prefix Length of the subnet"
-  value = "${nutanix_subnet.subnet.subnet_ip}/${nutanix_subnet.subnet.prefix_length}"
+  value       = nutanix_subnet.subnet.id
 }
--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,19 +1,1 @@
-output "name" {
-  description = "Name of the vm"
-  value = nutanix_virtual_machine.vm.name
-}
 
-output "vm_vCPU" {
-  description = "vCPU(s) of the vm"
-  value = "vCPU(s): ${nutanix_virtual_machine.vm.num_vcpus_per_socket * nutanix_virtual_machine.vm.num_sockets}"
-}
-
-output "vm_memory" {
-  description = "Memory of the vm"
-  value = "Memory: ${nutanix_virtual_machine.vm.memory_size_mib / 1024}GB"
-}
-
-output "vm_disk" {
-  description = "Disk of the vm"
-  value = "Disk: ${nutanix_virtual_machine.vm.disk_list[1].disk_size_mib / 1024}GB"
-}
